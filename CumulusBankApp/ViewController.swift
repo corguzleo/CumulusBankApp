96b1886c45e820dbd844a50028aa2b0763838e3c
Leonardo Cortes <cleonardo21@gmail.com>
2019-10-15 14:25:53 -0500

    Listas Ligadas

--- CumulusBankApp/ViewController.swift
+++ CumulusBankApp/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
     
     let goals = ["Tu respuesta","La educación de mis hijos","Mi patrimonio","Mi bienestar y el de mi familia"]
+    let monthlyAmount = ["3,000-5,000","*5,000-7,000*","7,000-10,000"]
+    let bankingAccount = ["Classic Savings Account","*MORRIS Education Plan*"]
     
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -18,11 +20,23 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return goals[row]
+        if pickerView.tag == 0 {
+            return goals[row]
+        }else if pickerView.tag == 1 {
+            return monthlyAmount[row]
+        }else{
+            return bankingAccount[row]
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return goals.count
+        if pickerView.tag == 0 {
+            return goals.count
+        }else if pickerView.tag == 1 {
+            return monthlyAmount.count
+        }else{
+            return bankingAccount.count
+        }
     }
     
     override func viewDidLoad() {
